8ea5cc5eeb85247ef9f6123820e112fe63c4bf2a
IsaacMorillo <irmorillowork@gmail.com>
2026-08-31 23:02:07 -0500

    Refactor playRound function to reduce code duplication
    Restructure round-evaluation logic to eliminate redundant code,
    shorten function length, and improve readability.

--- main.js
+++ main.js
@@ -6,13 +6,10 @@ function getComputerChoice() {
   switch (numRan) {
     case 1:
       return "paper";
-      break;
     case 2:
       return "rock";
-      break;
     case 3:
       return "scissors";
-      break;
   }
 }
 
@@ -24,48 +21,22 @@ function playRound(humanChoice, computerChoice) {
   humanChoice = humanChoice.toLowerCase();
   console.log(`Your choice: ${humanChoice}`);
   console.log(`Computer choice: ${computerChoice}`);
-  if (humanChoice === "paper") {
-    switch (computerChoice) {
-      case "paper":
-        console.log("You tied! You got the same thing as the computer");
-        break;
-      case "scissors":
-        console.log("You lose! Scissors beat Paper");
-        ++computerScore;
-        break;
-      case "rock":
-        console.log("You win! Paper beats Rock");
-        ++humanScore;
-        break;
-    }
-  } else if (humanChoice === "scissors") {
-    switch (computerChoice) {
-      case "scissors":
-        console.log("You tied! You got the same thing as the computer");
-        break;
-      case "rock":
-        console.log("You lose! Rock beats Scissors");
-        ++computerScore;
-        break;
-      case "paper":
-        console.log("You win! Scissors beat Paper");
-        ++humanScore;
-        break;
-    }
-  } else if (humanChoice === "rock") {
-    switch (computerChoice) {
-      case "rock":
-        console.log("You tied! You got the same thing as the computer");
-        break;
-      case "paper":
-        console.log("You lose! Paper beat Rock");
-        ++computerScore;
-        break;
-      case "scissors":
-        console.log("You win! Rock beats Scissors");
-        ++humanScore;
-        break;
-    }
+  if (
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log(`You win!! ${humanChoice} beats ${computerChoice}`);
+    ++humanScore;
+  } else if (
+    (humanChoice === "paper" && computerChoice === "scissors") ||
+    (humanChoice === "scissors" && computerChoice === "rock") ||
+    (humanChoice === "rock" && computerChoice === "paper")
+  ) {
+    console.log(`You lose. ${computerChoice} beat ${humanChoice}`);
+    ++computerScore;
+  } else if (humanChoice === computerChoice) {
+    console.log("You tied! You got the same thing as the computer");
   }
   console.log(`Your puntuation: ${humanScore}`);
   console.log(`Computer puntuation: ${computerScore}`);
@@ -73,6 +44,7 @@ function playRound(humanChoice, computerChoice) {
 
 function playGame() {
   for (let i = 1; i <= 5; i++) {
+    console.log("ROUND " + i + " !!");
     const player = getHumanChoice();
     const cpu = getComputerChoice();
     playRound(player, cpu);
