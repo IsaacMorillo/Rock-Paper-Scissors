ba87ce56dffc58c37abed7e806feb021e7b13ebf
IsaacMorillo <irmorillowork@gmail.com>
2026-08-31 22:37:13 -0500

    Create playRound function to evaluate round outcomes
    Implement the function to compare human and computer choices, declare
    the round winner, and update the respective score counter.

--- main.js
+++ main.js
@@ -5,19 +5,73 @@ function getComputerChoice() {
   const numRan = Math.ceil(Math.random() * 3);
   switch (numRan) {
     case 1:
-      return "Paper";
+      return "paper";
       break;
     case 2:
-      return "Rock";
+      return "rock";
       break;
     case 3:
-      return "Scissors";
+      return "scissors";
       break;
   }
 }
 
-function getHumanChoice(){
-    return prompt("Select between Rock, Paper or Scissors");
+function getHumanChoice() {
+  return prompt("Select between Rock, Paper or Scissors");
 }
 
-getHumanChoice();
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+  console.log(`Your choice: ${humanChoice}`);
+  console.log(`Computer choice: ${computerChoice}`);
+  if (humanChoice === "paper") {
+    switch (computerChoice) {
+      case "paper":
+        console.log("You tied! You got the same thing as the computer");
+        break;
+      case "scissors":
+        console.log("You lose! Scissors beat Paper");
+        ++computerScore;
+        break;
+      case "rock":
+        console.log("You win! Paper beats Rock");
+        ++humanScore;
+        break;
+    }
+  } else if (humanChoice === "scissors") {
+    switch (computerChoice) {
+      case "scissors":
+        console.log("You tied! You got the same thing as the computer");
+        break;
+      case "rock":
+        console.log("You lose! Rock beats Scissors");
+        ++computerScore;
+        break;
+      case "paper":
+        console.log("You win! Scissors beat Paper");
+        ++humanScore;
+        break;
+    }
+  } else if (humanChoice === "rock") {
+    switch (computerChoice) {
+      case "rock":
+        console.log("You tied! You got the same thing as the computer");
+        break;
+      case "paper":
+        console.log("You lose! Paper beat Rock");
+        ++computerScore;
+        break;
+      case "scissors":
+        console.log("You win! Rock beats Scissors");
+        ++humanScore;
+        break;
+    }
+  }
+  console.log(`Your puntuation: ${humanScore}`);
+  console.log(`Computer puntuation: ${computerScore}`);
+}
+
+let human = getHumanChoice();
+let com = getComputerChoice();
+
+playRound(human, com);
